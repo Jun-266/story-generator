f3dd771507867ec264b95a5c0de9a524d690b933
David Jun Kim <david.kim1@u.icesi.edu.co>
2023-10-06 23:32:39 -0500

    test: Pruebas para ER, caso "Si"

--- src/structures/regular_expression.py
+++ src/structures/regular_expression.py
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 import re
 class RegularExpression:
 
     def checkAnswer(chain, chain2):
         return re.search(chain,chain2)
+=======
+regular_expressions = {
+    'yes_or_no': r'((S|s)(I|i|í))|((N|n)(O|o))',
+    'path_choice': r'(C|c)amino\s?[123]:?\s?.*',
+}
+>>>>>>> 5f864b1 (test: Pruebas para ER, caso "Si")
---
+++ src/tests/regular_expression_test.py
@@ -0,0 +1,57 @@
+import re
+import unittest
+from src.structures.regular_expression import regular_expressions
+from src.ui.main import yes_options, no_options
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_yes_option(self):
+        re_for_yes = regular_expressions['yes_or_no']
+        input_1 = 'Si me gustaría continuar la historia'
+        input_2 = 'si quiero continuar con la historia.'
+        input_3 = 'Si, estoy de acuerdo'
+        input_4 = 'siiiiiiiiiiiii'
+        input_5 = 'Sí mi loco'
+        input_6 = 'sí, está bien'
+        input_7 = 'SI VOY A CONTINUAR'
+
+        match_obj_1 = re.match(re_for_yes, input_1)
+        match_obj_2 = re.match(re_for_yes, input_2)
+        match_obj_3 = re.match(re_for_yes, input_3)
+        match_obj_4 = re.match(re_for_yes, input_4)
+        match_obj_5 = re.match(re_for_yes, input_5)
+        match_obj_6 = re.match(re_for_yes, input_6)
+        match_obj_7 = re.match(re_for_yes, input_7)
+
+        my_tp_1 = match_obj_1.span()
+        my_tp_2 = match_obj_2.span()
+        my_tp_3 = match_obj_3.span()
+        my_tp_4 = match_obj_4.span()
+        my_tp_5 = match_obj_5.span()
+        my_tp_6 = match_obj_6.span()
+        my_tp_7 = match_obj_7.span()
+
+        process_str_1 = input_1[my_tp_1[0]: my_tp_1[1]]
+        process_str_2 = input_2[my_tp_2[0]: my_tp_2[1]]
+        process_str_3 = input_3[my_tp_3[0]: my_tp_3[1]]
+        process_str_4 = input_4[my_tp_4[0]: my_tp_4[1]]
+        process_str_5 = input_5[my_tp_5[0]: my_tp_5[1]]
+        process_str_6 = input_6[my_tp_6[0]: my_tp_6[1]]
+        process_str_7 = input_7[my_tp_7[0]: my_tp_7[1]]
+
+        self.assertEqual(process_str_1 in yes_options, True)
+        self.assertEqual(process_str_2 in yes_options, True)
+        self.assertEqual(process_str_3 in yes_options, True)
+        self.assertEqual(process_str_4 in yes_options, True)
+        self.assertEqual(process_str_5 in yes_options, True)
+        self.assertEqual(process_str_6 in yes_options, True)
+        self.assertEqual(process_str_7 in yes_options, True)
+
+    def test_no_option(self):
+        
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
